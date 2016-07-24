c04bb9efa376d23b99087f5b2fe8d77c715778ce
hwauck <hwauck@gmail.com>
2016-07-23 19:32:10 -0500

    added pregame survey

---
+++ Assets/pregameSurvey.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pregameSurvey : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	public void loadGame() {
+		// save gender, age, game experience, repeated plays to resultStr/database
+		Application.LoadLevel(2);
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
