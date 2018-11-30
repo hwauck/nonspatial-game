d868cf1d408aac75e346ee7f19d247edf15c013c
CherylYang97 <CherylYang97@users.noreply.github.com>
2018-11-30 00:48:12 -0600

    startRoom

--- Assets/playerStartRoom.cs
+++ Assets/playerStartRoom.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class playerStartRoom : MonoBehaviour {
 
@@ -14,6 +15,7 @@ public class playerStartRoom : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
+		//This part is for moving
 		if (Input.GetKeyDown (KeyCode.DownArrow)) {
 			if(this.transform.position.y==-6){
 				if(this.transform.position.x==-2.5||this.transform.position.x==1.5||this.transform.position.x==5.5) transform.position = new Vector3(transform.position.x, transform.position.y-2, transform.position.z);
@@ -35,7 +37,16 @@ public class playerStartRoom : MonoBehaviour {
 			}
 			else if(this.transform.position.x>-4.5&&this.transform.position.y!=-8&&this.transform.position.y!=6) transform.position = new Vector3(transform.position.x-2, transform.position.y, transform.position.z);
 		}
-
+		//This part is for going to other scenes
+		if(this.transform.position.x==-2.5&&this.transform.position.y==-8)  SceneManager.LoadScene("ice");
+		else if(this.transform.position.x==1.5&&this.transform.position.y==-8) SceneManager.LoadScene("ice_2");
+		else if(this.transform.position.x==5.5&&this.transform.position.y==-8) SceneManager.LoadScene("ice_3");
+		else if(this.transform.position.x==11.5&&this.transform.position.y==-4) SceneManager.LoadScene("ice_4");
+		else if(this.transform.position.x==11.5&&this.transform.position.y==2) SceneManager.LoadScene("ice_5");
+		// else if()
+		// else if()
+		// else if()
+		// else if()
 
 	}
 }
