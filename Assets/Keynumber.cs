95e181753fcf961ddb9df211ae8b36bd424c73c3
Li, Jinyuan <jinyuan2@illinois.edu>
2019-01-19 20:40:24 -0600

    Create Keynumber.cs
    added counter for key fragments

---
+++ Assets/Keynumber.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Keynumber : MonoBehaviour {
+    private DataCollector dataCollector;
+    Text keynum;
+
+	// Use this for initialization
+	void Start () {
+        dataCollector = GameObject.Find("DataCollector").GetComponent<DataCollector>();
+        keynum = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(dataCollector.ReportKeyNum() != 0 ){
+            keynum.text = "Number of key fragments obtained: " + dataCollector.ReportKeyNum();
+        }
+	}
+}
