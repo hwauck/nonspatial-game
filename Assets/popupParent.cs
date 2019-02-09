301b397a9f8b0fdf8168ac2a234fd162e9142e81
Li, Jinyuan <jinyuan2@illinois.edu>
2019-02-08 21:02:58 -0600

    Update popupParent.cs

--- Assets/popupParent.cs
+++ Assets/popupParent.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class popupParent : MonoBehaviour {
-    public GameObject levelNameParent;
+    /* this script is for hiding all the level names and key fragments counter text when the player presses P */
+    public GameObject levelNameParent; /* parent of all level names and key fragments counter */
 
 	// Use this for initialization
 	void Start () {
