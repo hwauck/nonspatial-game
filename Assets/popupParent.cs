2b26ece2096e40335b781acc9a98b1e0391851a7
Li, Jinyuan <jinyuan2@illinois.edu>
2019-02-08 20:54:08 -0600

    Create popupParent.cs

---
+++ Assets/popupParent.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class popupParent : MonoBehaviour {
+    public GameObject levelNameParent;
+    private DataCollector dataCollector;
+
+	// Use this for initialization
+	void Start () {
+        levelNameParent.SetActive(true);
+        dataCollector = GameObject.Find("DataCollector").GetComponent<DataCollector>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            levelNameParent.SetActive(false);
+            Debug.Log("set level texts to false !!!!!!!!!!!!!!!!!");
+        }
+	}
+}
