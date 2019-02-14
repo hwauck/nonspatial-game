f918361ebec639260d4bd0c0f823caad3f2b7bd5
Li, Jinyuan <jinyuan2@illinois.edu>
2019-02-14 16:35:52 -0600

    Update Keynumber.cs

--- Assets/Keynumber.cs
+++ Assets/Keynumber.cs
@@ -10,6 +10,8 @@ public class Keynumber : MonoBehaviour {
     public GameObject popmessage;
     private float mytimer;
     private bool haspoped;
+    private Image keyCounterPanel;
+    private Image messagePanel;
 	// Use this for initialization
 	void Start () {
         dataCollector = GameObject.Find("DataCollector").GetComponent<DataCollector>();
@@ -17,20 +19,26 @@ public class Keynumber : MonoBehaviour {
         popmessage.SetActive(false);
         mytimer = 0f;
         haspoped = false;
+
+        keyCounterPanel = GameObject.Find("keycounterBox").GetComponent<Image>();
+        messagePanel = GameObject.Find("messageBox").GetComponent<Image>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
         num = dataCollector.ReportKeyNum(); 
         if(num != 0 ){
+            keyCounterPanel.enabled = true;
             if(num == 9){    // change "9" in Keynumber.cs and popuptText2.cs
                 if(haspoped == false){
+                    messagePanel.enabled = true;
                     popmessage.SetActive(true);
                     mytimer += Time.deltaTime;
                 }
                 keynum.text = "Keys: 1"; 
                 if(mytimer > 4.0f){    // wait four second then make the message disappear
                     popmessage.SetActive(false);
+                    messagePanel.enabled = false;
                     mytimer = 0f;
                     haspoped = true;
                 }
@@ -42,3 +50,4 @@ public class Keynumber : MonoBehaviour {
         }
 	}
 }
+
